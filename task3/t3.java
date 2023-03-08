dbf13f6a5d18d581d3ca0bb661a29c87213992a1
Akkenton <akkenton.msk@gmail.com>
2023-03-09 00:35:17 +0300

    Оптимизирован код. Окончательный результат

--- task3/t3.java
+++ task3/t3.java
@@ -1,22 +1,28 @@
 package TASKS.task3;
 
 /**
- * 3
+ * Напишите метод, который принимает на вход строку (String) и определяет
+ * является ли строка палиндромом
+ * (возвращает boolean значение). Без встр. функций
  */
+
+// Сделан метод для строк без учета регистра
+
 public class t3 {
     public static void main(String[] args) {
         System.out.print("\033[H\033[J");
 
-        String str = "12221";
+        String str = "KABak";
+        System.out.println(str);
         System.out.println(palindrom(str));
     }
 
     static boolean palindrom(String str) {
         boolean flag = true;
-        StringBuffer strB = new StringBuffer(str);
+        str = str.toLowerCase();
 
-        for (int i = 0; i < strB.length(); i++) {
-            if (strB.charAt(i) == strB.charAt((strB.length() - 1) - i)) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt((str.length() - 1) - i)) {
                 continue;
             } else {
                 flag = false;
