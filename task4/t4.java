57f195c56801b666ae005d78238ba56bb4f0dd6c
Akkenton <akkenton.msk@gmail.com>
2023-03-09 23:06:40 +0300

    Решение задачи №4

--- task4/t4.java
+++ task4/t4.java
@@ -1,25 +1,48 @@
 package TASKS.task4;
 
 import java.util.logging.*;
+import java.io.IOException;
 import java.util.Scanner;
 
 // К калькулятору из предыдущего дз добавить логирование.  
+// делать дозапись в файл
 
 public class t4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SecurityException, IOException {
+
+        // основной блок
+
+        try (Scanner num = new Scanner(System.in, "Cp866")) {
+            Logger logger = Logger.getLogger(t4.class.getName());
+            logger.setLevel(Level.INFO);
+
+            FileHandler fh = new FileHandler("logcalc.txt");
+            logger.addHandler(fh);
+            SimpleFormatter sFormat = new SimpleFormatter();
+            fh.setFormatter(sFormat);
+
+            boolean contin = false;
+            do {
+                contin = CalcConstrBool(num, logger);
+            } while (contin);
+            num.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // метод расчетов у калькулятора из прошлого ДЗ
+    // добавлено логирование в конструкции switch и обработчик продолжения
+
+    static boolean CalcConstrBool(Scanner num, Logger logger) {
         System.out.println("\033[H\033[J");
-
-        Logger logger = Logger.getLogger(t4.class.getName());
-        logger.setLevel(Level.INFO);
-
-        Scanner num = new Scanner(System.in, "Cp866");
         System.out.println("Введите первое число: ");
         int count1 = num.nextInt();
         System.out.println("Введите операцию(*,+,/,-): ");
         var znak = num.next();
         System.out.println("Введите второе число: ");
         int count2 = num.nextInt();
-        num.close();
 
         int result;
         String resStr = new String();
@@ -29,7 +52,9 @@ public class t4 {
         switch (znak) {
             case "*":
                 result = count1 * count2;
-
+                resultStrB.append(result);
+                resStr = resultStrB.toString();
+                logger.info(resStr);
                 break;
             case "/":
                 result = count1 / count2;
@@ -54,5 +79,23 @@ public class t4 {
                 logger.info("Не верное выражение знака!");
                 break;
         }
+
+        boolean flag = false;
+        System.out.println("Продолжаем расчеты?( Y/N");
+        String decide = num.next();
+        decide = decide.toLowerCase();
+        switch (decide) {
+            case "y":
+                flag = true;
+                break;
+            case "n":
+                flag = false;
+                break;
+
+            default:
+                System.out.println("Прерывание цикла");
+                break;
+        }
+        return flag;
     }
 }
